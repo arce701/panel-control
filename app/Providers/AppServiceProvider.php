efb1ae50fd7aa0444fe9c2d066bac84a9fc8df84
arce701 <yarce701@gmail.com>
2020-05-23 18:40:32 -0500

    fixed https two

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if (env('REDIRECT_HTTPS')) {
-            dd($url->formatScheme('https://'));
             $url->formatScheme('https://');
         }
 

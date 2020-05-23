bd7819a4585bf8e87472f6d30d7708a3807cb909
arce701 <yarce701@gmail.com>
2020-05-23 18:14:17 -0500

    remove bootstrap.html

--- storage/framework/views/4aab32268080da1fd78fc9513d5bbb8acfe9114f.php
+++
@@ -1,7 +0,0 @@
-<div class="card">
-    <h4 class="card-header"><?php echo e($header); ?></h4>
-    <div class="card-body">
-        <?php echo e($slot); ?>
-
-    </div>
-</div><?php /**PATH D:\xampp\htdocs\panel\resources\views/shared/_card.blade.php ENDPATH**/ ?>

b20f4c5e6aba5a6db41bf6f785800064a1a85242
arce701 <yarce701@gmail.com>
2020-05-23 19:05:15 -0500

    add image and header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,13 @@
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>Laravel</h1>
-@php($secure='https://')
-@if(! is_null($secure))
-    {{  $secure ? 'https://' : 'http://' }}
-@endif
-</body>
-</html>
+@extends('layout')
+
+@section('title', "Crear usuario")
+
+@section('content')
+    @component('shared._card')
+        @slot('header', 'Welcome Panel Control')
+
+        <div class="container center-block" >
+            <img src="{{ asset('images/github-heroku.jpeg') }}" alt="" class="">
+        </div>
+    @endcomponent
+@endsection
